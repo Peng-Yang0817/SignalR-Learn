074791e0267b42b8f0a3e5e57cf3f58190d856f8
tcher2007 <tcher2007>
2024-05-11 22:48:40 +0800

    加入專案檔案。

---
+++ SignalRLearn/Ctrls/Index.js
@@ -0,0 +1,38 @@
+﻿var app = angular.module('myApp', []);
+app.controller('myController', function ($scope, $http, $timeout) {
+
+    $scope.title = "大家來聊天";
+    $scope.userName = "";
+    $scope.userMsg = "";
+    $scope.bindState = false;
+
+    function getbaseURL() {
+
+        var currentURL = window.location.href;
+        var url = new URL(currentURL);
+        var baseURL = url.origin;
+
+        return baseURL;
+    }
+
+
+
+    // 使用者名稱儲存
+    $scope.SaveUserName = function () {
+        var text = "用戶【" + $scope.userName + "】" + " 已加入聊天室. ";
+
+        mySignalHub_web.server.publishMsg(text);
+        $scope.bindState = true;
+    }
+
+    $scope.SendMsgAndClearMsgBox = function () {
+        // 給客戶端註冊方法，客戶端必須有方法來給Server 調用
+        // 這裡創建了 getMyMethod 方法給Server 調用，Client 接收到請求後，執行方法，將Sever 傳來的文字打印出來
+
+        var text = $scope.userName + " : " + $scope.userMsg;
+        // 調用Hub 提供的方法
+        mySignalHub_web.server.publishMsg(text);
+
+        $scope.userMsg = "";
+    }
+});
---
+++ SignalRLearn/MySignalHub.cs
@@ -0,0 +1,33 @@
+﻿    using Microsoft.AspNet.SignalR;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    namespace SignalRLearn
+    {
+        public class MySignalHub : Hub
+        {
+            /// <summary>
+            /// 總結
+            /// 上面的Hello() 方法是被客戶端調用的。
+            /// 下面的這個Clients.All.hello() 是Server 主動調客戶端的
+            /// </summary>
+            // Hub 內所有方法，都是要被客戶端調用的方法
+            public void Hello()
+            {
+                // Server 主動調用客戶端的方法
+                // 也就是所有客戶端都要有這個Hello 的方法可以被Server 調用
+                Clients.All.hello();
+            }
+            
+            // 等等Client 會打這個方法
+            public void PublishMsg(string txt)
+            {
+                // Server 接收到Client 傳來的訊息後，
+                //   直接調用Client 提供的clientMsgPublish 方法，並把txt 船過去
+                Clients.All.clientMsgPublish(txt);
+                
+            }
+        }
+    }
